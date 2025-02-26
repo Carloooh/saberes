5bf00f83f3e42e986ce025f9a67a037de0ac1690
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2025-02-26 15:35:47 -0300

    foto hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -32,22 +32,32 @@ const Hero = () => {
   }, []);
 
   if (loading) {
-    return <section className="px-2 pt-8">
-    <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center justify-between">
-      <div className="md:w-1/2 mb-8 md:mb-0">
-        <h2 className="text-3xl font-semibold text-gray-900 sm:text-4xl">Saberes</h2>
-        <p className="mt-4 text-gray-700 text-lg">
-          <strong>Visión:</strong> ...
-        </p>
-        <p className="mt-4 text-gray-700 text-lg">
-          <strong>Misión:</strong> ...
-        </p>
-      </div>
-      <div className="md:w-1/2 flex justify-center">
-        <Image src="/gato.png" width={500} height={300} alt="Gato" className="rounded shadow-lg" />
-      </div>
-    </div>
-  </section>;
+    return (
+      <section className="px-2 pt-8">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center justify-between">
+          <div className="md:w-1/2 mb-8 md:mb-0">
+            <h2 className="text-3xl font-semibold text-gray-900 sm:text-4xl">
+              Saberes
+            </h2>
+            <p className="mt-4 text-gray-700 text-lg">
+              <strong>Visión:</strong> ...
+            </p>
+            <p className="mt-4 text-gray-700 text-lg">
+              <strong>Misión:</strong> ...
+            </p>
+          </div>
+          <div className="md:w-1/2 flex justify-center">
+            <Image
+              src="/logo_saberes.webp"
+              width={500}
+              height={300}
+              alt="Gato"
+              className="rounded shadow-lg"
+            />
+          </div>
+        </div>
+      </section>
+    );
   }
 
   if (error) {
@@ -58,7 +68,9 @@ const Hero = () => {
     <section className="px-2 pt-8">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center justify-between">
         <div className="md:w-1/2 mb-8 md:mb-0">
-          <h2 className="text-3xl font-semibold text-gray-900 sm:text-4xl">Saberes</h2>
+          <h2 className="text-3xl font-semibold text-gray-900 sm:text-4xl">
+            Saberes
+          </h2>
           <p className="mt-4 text-gray-700 text-lg">
             <strong>Visión:</strong> {vision}
           </p>
@@ -67,7 +79,13 @@ const Hero = () => {
           </p>
         </div>
         <div className="md:w-1/2 flex justify-center">
-          <Image src="/gato.png" width={500} height={300} alt="Gato" className="rounded shadow-lg" />
+          <Image
+            src="/logo_saberes.webp"
+            width={500}
+            height={300}
+            alt="Gato"
+            className="rounded shadow-lg"
+          />
         </div>
       </div>
     </section>
