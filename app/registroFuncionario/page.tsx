67d434931d4beadc2e9bb03115c08f697fc1d6b7
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2025-03-03 17:29:57 -0300

    visual fix

--- app/registroFuncionario/page.tsx
+++ app/registroFuncionario/page.tsx
@@ -246,9 +246,6 @@ const RegistroFuncionario = () => {
               onChange={validateEmails}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#E91E63] focus:border-[#E91E63]"
             />
-            {emailError && (
-              <p className="mt-1 text-sm text-red-600">{emailError}</p>
-            )}
           </div>
           <div>
             <label className="block text-sm font-medium text-gray-700">
