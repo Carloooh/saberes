baa59891e786d9dde4367d869acb734f857acea5
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2025-03-10 13:24:20 -0300

    FEAT: timeout y WAL DB

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,10 @@
-import Database from 'better-sqlite3';
+import Database from "better-sqlite3";
 
-const db = new Database('db/database.sqlite', { verbose: console.log });
+const db = new Database("db/database.sqlite", {
+  verbose: console.log,
+  timeout: 10000,
+});
+
+db.exec("PRAGMA journal_mode = WAL;");
 
 export default db;
