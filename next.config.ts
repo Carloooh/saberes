926cecf8dea322cde4b5857f1c2a9388ce83785b
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2025-03-25 11:55:10 -0300

    docker/nginx settings

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
