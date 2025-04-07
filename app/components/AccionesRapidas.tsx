4dbc9751859536500eb71aa35b38b39b2e206d6b
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2025-04-07 00:14:17 -0400

    Mejora visual
    cambio de colores

--- app/components/AccionesRapidas.tsx
+++ app/components/AccionesRapidas.tsx
@@ -1,11 +1,41 @@
 const AccionesRapidas = () => {
   const items = [
-    { href: "/actividades", title: "ACTIVIDADES" },
-    { href: "/noticias", title: "NOTICIAS" },
-    { href: "/sedes", title: "SEDES" },
-    { href: "/galeria", title: "GALERÍA" },
-    { href: "/faq", title: "PREGUNTAS FRECUENTES" },
-    { href: "/matriculaEstudiante", title: "MATRICULAR ESTUDIANTE" }, // Nueva acción
+    {
+      href: "/actividades",
+      title: "ACTIVIDADES",
+      color: "from-blue-50 to-blue-100",
+      textColor: "text-blue-700 group-hover:text-blue-900",
+    },
+    {
+      href: "/noticias",
+      title: "NOTICIAS",
+      color: "from-green-50 to-green-100",
+      textColor: "text-green-700 group-hover:text-green-900",
+    },
+    {
+      href: "/sedes",
+      title: "SEDES",
+      color: "from-purple-50 to-purple-100",
+      textColor: "text-purple-700 group-hover:text-purple-900",
+    },
+    {
+      href: "/galeria",
+      title: "GALERÍA",
+      color: "from-amber-50 to-amber-100",
+      textColor: "text-amber-700 group-hover:text-amber-900",
+    },
+    {
+      href: "/faq",
+      title: "PREGUNTAS FRECUENTES",
+      color: "from-teal-50 to-teal-100",
+      textColor: "text-teal-700 group-hover:text-teal-900",
+    },
+    {
+      href: "/matriculaEstudiante",
+      title: "MATRICULAR ESTUDIANTE",
+      color: "from-rose-50 to-rose-100",
+      textColor: "text-rose-700 group-hover:text-rose-900",
+    }, // Nueva acción
   ];
 
   return (
@@ -23,10 +53,12 @@ const AccionesRapidas = () => {
           <a
             key={index}
             href={item.href}
-            className="group flex flex-col items-center justify-center h-32 bg-gradient-to-br from-indigo-50 to-indigo-100 
-                       rounded-lg shadow-md hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1"
+            className={`group flex flex-col items-center justify-center h-32 bg-gradient-to-br ${item.color} 
+                       rounded-lg shadow-md hover:shadow-lg transition-all duration-300 transform hover:-translate-y-1 border border-gray-100`}
           >
-            <span className="text-indigo-700 font-medium text-sm sm:text-base lg:text-lg group-hover:text-indigo-900 transition-colors duration-300 text-center">
+            <span
+              className={`font-medium text-sm sm:text-base lg:text-lg transition-colors duration-300 text-center ${item.textColor}`}
+            >
               {item.title}
             </span>
           </a>
